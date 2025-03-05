8a818661d059488d65b0205edea090e830a80b0f
Alessandro <galloalessandro77@gmail.com>
2025-03-05 17:21:02 +0100

    Added function get_stores_inventory

---
+++ backend/API-Stores/controllers/stores-inventory.js
@@ -0,0 +1,25 @@
+const asyncHandler = require("express-async-handler");
+const handleValidationErrors = require("./validation/validation.js");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.get_stores_inventory = asyncHandler(async (req, res) => {
+  handleValidationErrors(req, res);
+  try {
+    const storesWithInventoryAndItems = await prisma.store.findMany({
+      include: {
+        inventory: {
+          include: {
+            items: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({ storesWithInventoryAndItems });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while searching for stores" });
+  }
+});
