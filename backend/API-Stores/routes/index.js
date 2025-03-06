18ecad8d23d02df116cee2b8113f8c211d447f9a
Alessandro <galloalessandro77@gmail.com>
2025-03-06 17:43:33 +0100

    Fixed variable name

--- backend/API-Stores/routes/index.js
+++ backend/API-Stores/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const stores_inventory_controller = require("../controllers/store-inventory.js");
-const store_inventory_items_controller = require("../controllers/store-inventory-items.js");
+const new_store_inventory_items_controller = require("../controllers/store-inventory-items.js");
 const new_store_controller = require("../controllers/new-store.js");
 const delete_store_controller = require("../controllers/store.js");
 const delete_item_controller = require("../controller/delete-item.js");
@@ -13,7 +13,7 @@ router.get(
 
 router.post(
   "/new-store-inventory-items",
-  store_inventory_items_controller.post_store_inventory_items
+  new_store_inventory_items_controller.post_new_store_inventory_items
 );
 
 router.post("/new-store", new_store_controller.post_new_store);
