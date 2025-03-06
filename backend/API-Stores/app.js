b654a03d3f02596e6940c34434001abc224d4cc5
Alessandro <galloalessandro77@gmail.com>
2025-03-06 18:41:32 +0100

    Changed PORT

--- backend/API-Stores/app.js
+++ backend/API-Stores/app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`Authentication is running on port ${PORT}`);
