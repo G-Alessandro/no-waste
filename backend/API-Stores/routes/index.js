6c88cf0ed9ea5ba84f861d8d73ee3ce1a16f2369
Alessandro <galloalessandro77@gmail.com>
2025-03-06 15:17:17 +0100

    Added routes

--- backend/API-Stores/routes/index.js
+++ backend/API-Stores/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const stores_inventory_controllers = require("./controllers/store-inventory.js");
+const store_inventory_items_controllers = require("./controllers/store-inventory-items.js");
+const new_store_controllers = require("./controllers/new-store.js");
+const delete_store_controllers = require("./controllers/store.js");
+const delete_item_controllers = require("./controller/delete-item.js");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  "/stores-inventory",
+  stores_inventory_controllers.get_stores_inventory
+);
+
+router.post(
+  "/new-store-inventory-items",
+  store_inventory_items_controllers.post_store_inventory_items
+);
+
+router.post("/new-store", new_store_controllers.post_new_store);
+
+router.delete("/delete-store", delete_store_controllers.delete_store);
+
+router.delete("/delete-item", delete_item_controllers.delete_item);
 
 module.exports = router;
