57fee348dcd1d125722ff82978d39f3cb88372d5
Alessandro <galloalessandro77@gmail.com>
2025-03-06 18:43:42 +0100

    Added app.js

---
+++ backend/API-Users/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 5003;
+
+app.listen(PORT, () => {
+  console.log(`Authentication is running on port ${PORT}`);
+});
+
+module.exports = app;
