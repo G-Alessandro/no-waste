a74794a8882dbd86a1d0a21993d9a506519378bd
Alessandro <galloalessandro77@gmail.com>
2025-03-12 17:42:57 +0100

    Added home-page route

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/home-page/HomePage.jsx";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
