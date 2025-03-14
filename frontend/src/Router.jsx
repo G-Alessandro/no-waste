7fb96500ffda6ec1f63c7bffaa4a16ff91533e7c
Alessandro <galloalessandro77@gmail.com>
2025-03-14 14:17:24 +0100

    Added finds-stores route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/home-page/HomePage.jsx";
+import FindStores from "./components/finds-stores-page/FindStore.jsx";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/finds-stores",
+      element: <FindStores />,
     },
   ]);
   return <RouterProvider router={router} />;
