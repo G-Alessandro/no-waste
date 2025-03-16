4402d6de97cf459fe7e9971c4ca7b240b9d72697
Alessandro <galloalessandro77@gmail.com>
2025-03-16 15:31:08 +0100

    Added function AddStore

---
+++ frontend/src/components/finds-stores-page/add-store/AddStore.jsx
@@ -0,0 +1,51 @@
+export default function AddStore({
+  newStoreLatitude,
+  newStoreLongitude,
+  userId,
+  setMessage,
+  setError,
+}) {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      storeName: event.target["store-name"].value,
+      latitude: newStoreLatitude,
+      longitude: newStoreLongitude,
+      userId: userId,
+    };
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/new-store`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          mode: "cors",
+          credentials: "include",
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setMessage(data.message);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="store-name">Store Name</label>
+      <input
+        type="text"
+        id="store-name"
+        name="store-name"
+        minLength={1}
+        maxLength={30}
+        placeholder="Enter the store name"
+        required
+      />
+      <button type="submit">ADD STORE</button>
+    </form>
+  );
+}
