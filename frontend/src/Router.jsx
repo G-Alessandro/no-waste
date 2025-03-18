9e2cb812120789a2f56d09d4bbc433109c516c6c
Alessandro <galloalessandro77@gmail.com>
2025-03-18 13:22:47 +0100

    Added registration and login route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/home-page/HomePage.jsx";
 import FindStores from "./components/finds-stores-page/FindStore.jsx";
+import Registration from "./components/registration-page/Registration.jsx";
+import Login from "./components/login-page/Login.jsx";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -12,6 +14,11 @@ const Router = () => {
       path: "/finds-stores",
       element: <FindStores />,
     },
+    {
+      path: "/registration",
+      element: <Registration />,
+    },
+    { path: "/login", element: <Login /> },
   ]);
   return <RouterProvider router={router} />;
 };
