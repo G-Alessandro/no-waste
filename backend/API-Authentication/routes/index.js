906e80db311dabff6f7bdee0a9f99244a57f5d87
Alessandro <galloalessandro77@gmail.com>
2025-03-26 18:59:55 +0100

    Added routes refresh-token and logout Fixed controllers path

--- backend/API-Authentication/routes/index.js
+++ backend/API-Authentication/routes/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const login_controllers = require("./controllers/login.js");
-const registration_controllers = require("./controllers/registration.js");
-const demo_account_controllers = require("./controllers/demo-account.js");
+const login_controllers = require("../controllers/login.js");
+const registration_controllers = require("../controllers/registration.js");
+const refresh_token_controllers = require("../controllers/refreshToken.js");
+const demo_account_controllers = require("../controllers/demo-account.js");
+const logout_controllers = require("../controllers/logout.js");
 
 router.post("/login", login_controllers.login_post);
 
 router.post("/registration", registration_controllers.registration_post);
 
+router.post("/refresh-token", refresh_token_controllers.refresh_token_post);
+
 router.get("/demo-account", demo_account_controllers.demo_account_get);
+
+router.post("/logout", logout_controllers.logout_post);
