41309f2208dee57823ba4abd6502d8edebb95f13
Alessandro <galloalessandro77@gmail.com>
2025-03-28 19:02:24 +0100

    Added tokenCleaner.js

---
+++ backend/API-Authentication/utils/tokenCleaner.js
@@ -0,0 +1,31 @@
+const cron = require("node-cron");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const deleteExpiredTokens = async () => {
+  try {
+    const now = new Date();
+    const deletedTokens = await prisma.refreshToken.deleteMany({
+      where: {
+        expiresAt: { lt: now },
+      },
+    });
+
+    if (deletedTokens.count > 0) {
+      console.log(`Removed ${deletedTokens.count} expired tokens.`);
+    }
+  } catch (error) {
+    console.error("Error clearing expired tokens:", error);
+  }
+};
+
+const startExpiredTokensCleanup = () => {
+  cron.schedule("0 0 * * *", async () => {
+    console.log("Start cleaning expired tokens...");
+    await deleteExpiredTokens();
+  });
+
+  console.log("Cron job active: clean expired tokens every day at midnight..");
+};
+
+module.exports = { startExpiredTokensCleanup };
