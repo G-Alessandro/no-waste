f02cae987027471726418ad66d2c902152d35fc7
Alessandro <galloalessandro77@gmail.com>
2025-03-31 17:31:15 +0200

    Added requiresAuthentication key

--- backend/API-Gateway/services/index.js
+++ backend/API-Gateway/services/index.js
@@ -2,10 +2,12 @@ const services = [
   {
     route: "/authentication",
     target: process.env.AUTHENTICATION_SERVICES,
+    requiresAuthentication: false,
   },
   {
     route: "/users",
     target: process.env.USERS_SERVICES,
+    requiresAuthentication: true,
   },
 ];
 
