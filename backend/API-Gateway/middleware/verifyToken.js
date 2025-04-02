ee80a62d082ccbf55ee2bd0b8a43622f3b0c0cf1
Alessandro <galloalessandro77@gmail.com>
2025-04-02 16:18:02 +0200

    Added function refreshAccessToken and fixed the verifyToken function to integrate it

--- backend/API-Gateway/middleware/verifyToken.js
+++ backend/API-Gateway/middleware/verifyToken.js
@@ -1,21 +1,58 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token)
-    return res.status(403).json({
+const refreshAccessToken = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    const response = await fetch(
+      `${process.env.AUTHENTICATION_SERVICES}/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `refreshToken=${refreshToken}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      console.log(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const newAccessToken = data.accessToken;
+
+    req.headers["authorization"] = `Bearer ${newAccessToken}`;
+    next();
+  } catch (error) {
+    console.error(
+      "Error refreshing token:",
+      error.response?.data || error.message
+    );
+    return res
+      .status(401)
+      .json({ message: "Session expired. Please log in again." });
+  }
+};
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader)
+    res.status(403).json({
       message: "Access denied, you must log in to access this feature",
     });
+  const token = authHeader.split(" ")[1];
 
-  jwt.verify(
-    token.split(" ")[1],
-    process.env.JWT_SECRET_KEY,
-    (err, decoded) => {
-      if (err) return res.status(401).json({ message: "Invalid token" });
-      req.user = decoded;
-      next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      console.log("Login data expired, trying to refresh...");
+      return await refreshAccessToken(req, res, next);
     }
-  );
+    res.status(401).json({ message: "Invalid login data" });
+  }
 };
 
 module.exports = { verifyToken };
