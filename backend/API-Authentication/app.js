8bcb1928598f1a569bea2cde43477110f65fea32
Alessandro <galloalessandro77@gmail.com>
2025-04-12 19:21:32 +0200

    Removed cors Added verifyGatewaySecret

--- backend/API-Authentication/app.js
+++ backend/API-Authentication/app.js
@@ -1,30 +1,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require("cors");
 const helmet = require("helmet");
 const { startExpiredTokensCleanup } = require("./utils/tokenCleaner");
+const verifyGatewaySecret = require("./middleware/verifyGatewaySecret");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
 const app = express();
 
 app.set("trust proxy", true);
-
-app.use(
-  cors({
-    origin: allowedOrigin,
-    credentials: true,
-  })
-);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(helmet());
+app.use("/authentication", verifyGatewaySecret);
 
 app.use("/", indexRouter);
 
