9a7586ba90f126a8ae4a55e6e34c24587d4f1ede
Alessandro <galloalessandro77@gmail.com>
2025-04-14 12:42:47 +0200

    Fixed logout logic

--- backend/API-Authentication/controllers/logout.js
+++ backend/API-Authentication/controllers/logout.js
@@ -1,29 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const handleValidationErrors = require("./validation/validation");
+const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 exports.logout_post = asyncHandler(async (req, res) => {
   handleValidationErrors(req, res);
   try {
-    const refreshToken = req.cookies.refreshToken;
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Login data not found" });
+    const accessToken = req.body.accessToken;
+
+    if (!accessToken) {
+      res.status(401).json({ message: "Login data not found" });
     }
 
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+
     const deletedToken = await prisma.refreshToken.deleteMany({
-      where: { token: refreshToken },
+      where: { userId: decoded.userId },
     });
 
     if (deletedToken.count === 0) {
-      return res.status(403).json({ message: "Invalid login data" });
+      res.status(403).json({ message: "Invalid login data" });
     }
-
-    res.clearCookie("refreshToken", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-    });
 
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
