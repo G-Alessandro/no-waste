09a2d06460aeddb6ade386a78f48def0dc1d4750
Alessandro <galloalessandro77@gmail.com>
2025-04-15 15:59:57 +0200

    Added function fetchUserData and handleLogout

--- frontend/src/components/top-bar/TopBar.jsx
+++ frontend/src/components/top-bar/TopBar.jsx
@@ -1,10 +1,92 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function TopBar() {
+  const [userData, setUserData] = useState(null);
+  const [showLoader, setShowLoader] = useState(false);
+  const [logoutMessage, setLogoutMessage] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("accessToken"));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("accessToken"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!token) return;
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/authentication/user-data`,
+          {
+            method: "GET",
+            credentials: "include",
+            mode: "cors",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        if (!data) {
+          console.log("No data found");
+        } else {
+          setUserData(data.userData);
+        }
+      } catch (error) {
+        console.log("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, [token]);
+
+  const handleLogout = async (event) => {
+    event.preventDefault();
+    setShowLoader(true);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/authentication/logout`,
+        {
+          method: "POST",
+          credentials: "include",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            accessToken: localStorage.getItem("accessToken"),
+          }),
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        setLogoutMessage(data.error);
+      } else {
+        localStorage.removeItem("accessToken");
+        setUserData(null);
+        setToken(null);
+        setLogoutMessage(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setShowLoader(false);
+      setTimeout(() => setLogoutMessage(null), 5000);
+    }
+  };
+
   return (
     <header>
       <Link>
-        <img src="" alt="" />
         <h1>NoWaste</h1>
       </Link>
       <nav>
@@ -15,13 +97,38 @@ export default function TopBar() {
           <li>
             <Link to="/finds-stores">FINDS STORES</Link>
           </li>
-          <li>
-            <Link to="/login">SIGN-IN</Link>
-          </li>
-          <li>
-            <Link to="/registration">SIGN-UP</Link>
-          </li>
+          {!userData && (
+            <>
+              <li>
+                <Link to="/login">SIGN-IN</Link>
+              </li>
+              <li>
+                <Link to="/registration">SIGN-UP</Link>
+              </li>
+            </>
+          )}
+          {userData && (
+            <>
+              <li>
+                <p>{`${userData.firstName} ${userData.lastName}`}</p>
+              </li>
+              <li>
+                {!showLoader && (
+                  <button onClick={handleLogout} type="button">
+                    Logout
+                  </button>
+                )}
+                {showLoader && (
+                  <div
+                    aria-live="polite"
+                    aria-label="Logging out, please wait..."
+                  ></div>
+                )}
+              </li>
+            </>
+          )}
         </ul>
+        {logoutMessage && <p aria-live="polite">{logoutMessage}</p>}
       </nav>
     </header>
   );
