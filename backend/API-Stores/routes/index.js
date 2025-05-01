3ff4eac86de75c5bce3116454fbd8cdcf50dedca
Alessandro <galloalessandro77@gmail.com>
2025-05-01 18:57:32 +0200

    Added get items list route and routes params

--- backend/API-Stores/routes/index.js
+++ backend/API-Stores/routes/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const stores_inventory_controller = require("../controllers/store-inventory.js");
-const new_store_inventory_items_controller = require("../controllers/store-inventory-items.js");
+const get_stores_inventory_controller = require("../controllers/get-stores-inventory.js");
+const get_items_list_controller = require("../controllers/get-items-list.js");
+const new_store_inventory_items_controller = require("../controllers/new-store-inventory-items.js");
 const new_store_controller = require("../controllers/new-store.js");
-const delete_store_controller = require("../controllers/store.js");
-const delete_item_controller = require("../controller/delete-item.js");
+const delete_store_controller = require("../controllers/delete-store.js");
+const delete_item_controller = require("../controllers/delete-item.js");
 
 router.get(
   "/stores-inventory",
-  stores_inventory_controller.get_stores_inventory
+  get_stores_inventory_controller.get_stores_inventory
 );
+
+router.get("/items-list/:id", get_items_list_controller.get_items_list);
 
 router.post(
   "/new-store-inventory-items",
@@ -18,8 +21,8 @@ router.post(
 
 router.post("/new-store", new_store_controller.post_new_store);
 
-router.delete("/delete-store", delete_store_controller.delete_store);
+router.delete("/delete-store/:id", delete_store_controller.delete_store);
 
-router.delete("/delete-item", delete_item_controller.delete_item);
+router.delete("/delete-item/:id", delete_item_controller.delete_item);
 
 module.exports = router;
