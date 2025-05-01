0a9bbe924f3b961cad949e55e507a0b6dd118efe
Alessandro <galloalessandro77@gmail.com>
2025-05-01 18:50:00 +0200

    Added get items list

---
+++ backend/API-Stores/controllers/get-items-list.js
@@ -0,0 +1,59 @@
+const asyncHandler = require("express-async-handler");
+const { param } = require("express-validator");
+const handleValidationErrors = require("./validation/validation.js");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.get_items_list = [
+  param("storeId").isInt().trim().escape(),
+  asyncHandler(async (req, res) => {
+    handleValidationErrors(req, res);
+    try {
+      const inventoryId = await prisma.inventory.findUnique({
+        where: { storeId: Number(req.params.storeId) },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!inventoryId) {
+        return res.status(200).json({
+          message:
+            "The store currently has no merchandise that is about to expire",
+        });
+      }
+
+      const items = await prisma.item.findMany({
+        where: { inventoryId: inventoryId.id },
+        select: {
+          id: true,
+          name: true,
+          type: true,
+          productionDate: true,
+          expirationDate: true,
+          price: true,
+          createdByUserId: true,
+        },
+      });
+
+      const itemsList = items.map((item) => {
+        const daysRemaining = Math.round(
+          (new Date(item.expirationDate) - new Date(item.productionDate)) /
+            (1000 * 60 * 60 * 24)
+        );
+        const { productionDate, ...rest } = item;
+        return {
+          ...rest,
+          daysRemaining,
+        };
+      });
+
+      res.status(200).json({ itemsList });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while searching for stores" });
+    }
+  }),
+];
