88e9d5016a6f56fe8dfc063209d3789afcedaa58
Alessandro <galloalessandro77@gmail.com>
2025-05-06 18:25:35 +0200

    Changed path name and variable

--- backend/API-Stores/routes/index.js
+++ backend/API-Stores/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const get_stores_inventory_controller = require("../controllers/get-stores-inventory.js");
+const get_stores_list_controller = require("../controllers/get-stores-list.js");
 const get_items_list_controller = require("../controllers/get-items-list.js");
 const new_store_inventory_items_controller = require("../controllers/new-store-inventory-items.js");
 const new_store_controller = require("../controllers/new-store.js");
@@ -8,8 +8,8 @@ const delete_store_controller = require("../controllers/delete-store.js");
 const delete_item_controller = require("../controllers/delete-item.js");
 
 router.get(
-  "/stores-inventory",
-  get_stores_inventory_controller.get_stores_inventory
+  "/stores-list",
+  get_stores_list_controller.get_stores_list
 );
 
 router.get("/items-list/:id", get_items_list_controller.get_items_list);
