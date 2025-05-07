df0fa912db0685380b685dd6f3c5d51ffd268aeb
Alessandro <galloalessandro77@gmail.com>
2025-05-07 17:06:24 +0200

    Changed file name Added grouping by food type

---
+++ backend/API-Stores/controllers/get-stores-list.js
@@ -0,0 +1,63 @@
+const asyncHandler = require("express-async-handler");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.get_stores_list = asyncHandler(async (req, res) => {
+  try {
+    const storesWithItems = await prisma.store.findMany({
+      include: {
+        inventory: {
+          include: {
+            items: {
+              select: {
+                type: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const storesList = storesWithItems.map((store) => {
+      const items = store.inventory?.items ?? [];
+      let freshFoods = [];
+      let cannedFoods = [];
+
+      const typeCounts = items.reduce((acc, item) => {
+        acc[item.type] = (acc[item.type] || 0) + 1;
+        return acc;
+      }, {});
+
+      Object.entries(typeCounts).forEach(([key, value]) => {
+        const entry = { key, quantity: value };
+        let type = {
+          type: entry.key,
+          quantity: entry.quantity,
+        };
+
+        if (key.startsWith("canned")) {
+          cannedFoods.push(type);
+        } else {
+          freshFoods.push(type);
+        }
+      });
+
+      return {
+        id: store.id,
+        name: store.name,
+        latitude: store.latitude,
+        longitude: store.longitude,
+        createdByUserId: store.createdByUserId,
+        freshFoods,
+        cannedFoods,
+      };
+    });
+
+    res.status(200).json({ storesList });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while searching for stores" });
+  }
+});
