cf31cf676a6dad3ea0ef5734e536a21e22e03741
Alessandro <galloalessandro77@gmail.com>
2025-05-07 18:16:25 +0200

    Fixed items-list param

--- backend/API-Stores/routes/index.js
+++ backend/API-Stores/routes/index.js
@@ -7,12 +7,9 @@ const new_store_controller = require("../controllers/new-store.js");
 const delete_store_controller = require("../controllers/delete-store.js");
 const delete_item_controller = require("../controllers/delete-item.js");
 
-router.get(
-  "/stores-list",
-  get_stores_list_controller.get_stores_list
-);
+router.get("/stores-list", get_stores_list_controller.get_stores_list);
 
-router.get("/items-list/:id", get_items_list_controller.get_items_list);
+router.get("/items-list/:storeId", get_items_list_controller.get_items_list);
 
 router.post(
   "/new-store-inventory-items",
