0525cae19e121e0bc2b3b03de2d785b9ff6de666
Alessandro <galloalessandro77@gmail.com>
2025-05-09 17:06:32 +0200

    Added function App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from "react";
+import React, { StrictMode, createContext, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./Router.jsx";
 
+export const AuthContext = createContext(null);
+export const statusContext = createContext(null);
+
+function App() {
+  const [token, setToken] = useState(localStorage.getItem("accessToken"));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("accessToken"));
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      <Router />
+    </AuthContext.Provider>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <App />
   </StrictMode>
 );
