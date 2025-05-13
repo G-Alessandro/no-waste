fd416e51c4ac29260f475ea4ccaed1cb74bece1b
Alessandro <galloalessandro77@gmail.com>
2025-05-13 16:41:13 +0200

    Removed userId validation Added jwt

--- backend/API-Stores/controllers/new-store.js
+++ backend/API-Stores/controllers/new-store.js
@@ -4,21 +4,37 @@ const handleValidationErrors = require("./validation/validation.js");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const he = require("he");
+const jwt = require("jsonwebtoken");
 
 exports.post_new_store = [
   body("storeName").isLength({ min: 1, max: 30 }).trim().escape(),
   body("latitude").isFloat().trim().escape(),
   body("longitude").isFloat().trim().escape(),
-  body("userId").isInt().trim().escape(),
   asyncHandler(async (req, res) => {
     handleValidationErrors(req, res);
+
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      res.status(403).json({
+        message:
+          "Unauthorized deletion, you must log in to access this feature",
+      });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
     try {
-      const newStore = await prisma.store.create({
+
+      const decodedJwt = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const userId = decodedJwt.userId;
+
+      await prisma.store.create({
         data: {
           name: he.decode(req.body.storeName),
           latitude: parseFloat(req.body.latitude),
           longitude: parseFloat(req.body.longitude),
-          createdByUserId: Number(req.body.userId),
+          createdByUserId: userId,
         },
       });
       res.status(201).json({ message: "The new store has been added" });
