7754525f28776173f6c0f4ac9ed13bfa207fdbea
Alessandro <galloalessandro77@gmail.com>
2025-05-19 17:35:46 +0200

    Fixed typo Uncommented demo account

--- backend/API-Authentication/routes/index.js
+++ backend/API-Authentication/routes/index.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const login_controllers = require("../controllers/login.js");
-const registration_controllers = require("../controllers/registration.js");
-const refresh_token_controllers = require("../controllers/refreshToken.js");
-// const demo_account_controllers = require("../controllers/demo-account.js");
-const user_data_controllers = require("../controllers/userData.js");
-const logout_controllers = require("../controllers/logout.js");
+const login_controller = require("../controllers/login.js");
+const registration_controller = require("../controllers/registration.js");
+const refresh_token_controller = require("../controllers/refreshToken.js");
+const demo_account_controller = require("../controllers/demo-account.js");
+const user_data_controller = require("../controllers/userData.js");
+const logout_controller = require("../controllers/logout.js");
 
-router.post("/login", login_controllers.login_post);
+router.post("/login", login_controller.login_post);
 
-router.post("/registration", registration_controllers.registration_post);
+router.post("/registration", registration_controller.registration_post);
 
-router.post("/refresh-token", refresh_token_controllers.refresh_token_post);
+router.post("/refresh-token", refresh_token_controller.refresh_token_post);
 
-// router.get("/demo-account", demo_account_controllers.demo_account_get);
+router.get("/demo-account", demo_account_controller.demo_account_get);
 
-router.get("/user-data", user_data_controllers.user_data_get);
+router.get("/user-data", user_data_controller.user_data_get);
 
-router.post("/logout", logout_controllers.logout_post);
+router.post("/logout", logout_controller.logout_post);
 
 module.exports = router;
