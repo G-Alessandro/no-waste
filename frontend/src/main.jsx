e6075f6d84550a992769f46564027b542265e6bf
Alessandro <galloalessandro77@gmail.com>
2025-06-12 16:11:19 +0200

    Removed createContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,10 @@
-import React, { StrictMode, createContext, useState, useEffect } from "react";
+import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import Router from "./Router.jsx";
 
-export const AuthContext = createContext(null);
-export const statusContext = createContext(null);
-
-function App() {
-  const [token, setToken] = useState(localStorage.getItem("accessToken"));
-
-  useEffect(() => {
-    const handleStorageChange = () => {
-      setToken(localStorage.getItem("accessToken"));
-    };
-    window.addEventListener("storage", handleStorageChange);
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
-
-  return (
-    <AuthContext.Provider value={{ token, setToken }}>
-      <Router />
-    </AuthContext.Provider>
-  );
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>
 );
