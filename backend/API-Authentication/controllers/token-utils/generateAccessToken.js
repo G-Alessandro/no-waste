fd6b29639f527840a0540d0632d436906226d2ff
Alessandro <galloalessandro77@gmail.com>
2025-06-18 18:18:44 +0200

    Added JWT_EXPIRATION env

--- backend/API-Authentication/controllers/token-utils/generateAccessToken.js
+++ backend/API-Authentication/controllers/token-utils/generateAccessToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (userId) => {
   return jwt.sign({ userId: userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "15m",
+    expiresIn: process.env.JWT_EXPIRATION,
   });
 };
 
