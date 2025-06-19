0a2882378f49557d9d9a0cab43379d7aac73ebdc
Alessandro <galloalessandro77@gmail.com>
2025-06-19 17:31:18 +0200

    Fixed access token and refresh token verification and update

--- backend/API-Gateway/middleware/verifyToken.js
+++ backend/API-Gateway/middleware/verifyToken.js
@@ -2,27 +2,34 @@ const jwt = require("jsonwebtoken");
 
 const refreshAccessToken = async (req, res, next) => {
   try {
-    const refreshToken = req.cookies.refreshToken;
+    
     const response = await fetch(
       `${process.env.AUTHENTICATION_SERVICES}/refresh-token`,
       {
         method: "POST",
         credentials: "include",
+        mode: "cors",
         headers: {
           "Content-Type": "application/json",
-          Cookie: `refreshToken=${refreshToken}`,
+          Cookie: `refreshToken=${req.cookies.refreshToken}`,
         },
       }
     );
-    if (!response.ok) {
-      console.log(`HTTP error! Status: ${response.status}`);
-    }
 
     const data = await response.json();
-    const newAccessToken = data.accessToken;
 
-    req.headers["authorization"] = `Bearer ${newAccessToken}`;
-    next();
+    const setCookie = response.headers.get("set-cookie");
+    if (setCookie) {
+      res.setHeader('Set-Cookie', setCookie);
+    }
+
+    if (response.ok && data?.accessToken) {
+      req.headers["authorization"] = data.accessToken;
+      res.setHeader("Authorization", data.accessToken);
+      res.setHeader("Access-Control-Expose-Headers", "Authorization");
+    }
+
+    return next();
   } catch (error) {
     console.error(
       "Error refreshing token:",
@@ -36,22 +43,25 @@ const refreshAccessToken = async (req, res, next) => {
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader)
-    res.status(403).json({
+
+  if (!authHeader?.startsWith("Bearer "))
+    return res.status(401).json({
       message: "Access denied, you must log in to access this feature",
     });
+
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = decoded;
+    jwt.verify(token, process.env.JWT_SECRET_KEY);
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError") {
-      console.log("Login data expired, trying to refresh...");
-      return await refreshAccessToken(req, res, next);
+      console.log("Access token expired, refreshing...");
+
+      return refreshAccessToken(req, res, next);
     }
-    res.status(401).json({ message: "Invalid login data" });
+
+    return res.status(401).json({ error: "Invalid login data" });
   }
 };
 
