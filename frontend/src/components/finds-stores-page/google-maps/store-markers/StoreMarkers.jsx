f17f832a9bea570e21b5aa8fa4891b40eeef7616
Alessandro <galloalessandro77@gmail.com>
2025-07-07 13:40:14 +0200

    Added StoreMarkers

---
+++ frontend/src/components/finds-stores-page/google-maps/store-markers/StoreMarkers.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import {
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+  useMapsLibrary,
+} from "@vis.gl/react-google-maps";
+import FoodTypeCounter from "../../store-list/food-type-counter/FoodTypeCounter";
+
+export default function StoreMarkers({
+  storesList,
+  selectedStore,
+  setSelectedStore,
+}) {
+  const geocodingApiLoaded = useMapsLibrary("geocoding");
+  const [geocodingService, setGeocodingService] = useState(null);
+  const [storeInfo, setStoreInfo] = useState(null);
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+
+  useEffect(() => {
+    if (!geocodingApiLoaded) return;
+    setGeocodingService(new window.google.maps.Geocoder());
+  }, [geocodingApiLoaded]);
+
+  const handleClick = (store) => {
+    setSelectedStore({
+      storeId: store.id,
+      storeName: store.name,
+      location: {
+        lat: store.latitude,
+        lng: store.longitude,
+      },
+      freshFoods: store.freshFoods,
+      cannedFoods: store.cannedFoods,
+    });
+  };
+
+  useEffect(() => {
+    if (!geocodingService || !selectedStore) return;
+
+    const newStoreLatLng = {
+      lat: selectedStore.location.lat,
+      lng: selectedStore.location.lng,
+    };
+
+    geocodingService.geocode(
+      { location: newStoreLatLng },
+      (results, status) => {
+        if (results && status === "OK") {
+          const storeAddress = results[0].formatted_address;
+          setStoreInfo({
+            storeName: selectedStore.storeName,
+            freshFoods: selectedStore.freshFoods,
+            cannedFoods: selectedStore.cannedFoods,
+            storeAddress,
+          });
+
+          setInfoWindowShown(true);
+        }
+      }
+    );
+  }, [selectedStore]);
+
+  return (
+    <>
+      {storesList.map((store) => (
+        <AdvancedMarker
+          key={store.id}
+          position={{
+            lat: store.latitude,
+            lng: store.longitude,
+          }}
+          clickable={true}
+          onClick={() => handleClick(store)}
+        >
+          <Pin
+            background={"#b7dbff"}
+            glyphColor={"#000"}
+            borderColor={"#000"}
+          />
+        </AdvancedMarker>
+      ))}
+
+      {infoWindowShown && selectedStore && storeInfo && (
+        <InfoWindow
+          position={selectedStore.location}
+          pixelOffset={[0, -30]}
+          onClose={() => setInfoWindowShown(false)}
+        >
+          <h2>{storeInfo.storeName}</h2>
+          <FoodTypeCounter
+            freshFoods={storeInfo.freshFoods}
+            cannedFoods={storeInfo.cannedFoods}
+          />
+          <p>{storeInfo.storeAddress}</p>
+        </InfoWindow>
+      )}
+    </>
+  );
+}
