87e94215c05008594f60c9a8fa350e204f6374d0
Alessandro <galloalessandro77@gmail.com>
2025-07-13 12:02:04 +0200

    Added options Moved to user-location

---
+++ frontend/src/components/finds-stores-page/user-locations/user-geolocation/UserGeolocation.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function UserGeolocation({ setUserLocation }) {
+  const [geolocationError, setGeolocationError] = useState(false);
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      };
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        },
+        options
+      );
+    } else {
+      setGeolocationError(true);
+      console.error("Geolocation is not supported by this browser.");
+      setTimeout(() => setGeolocationError(false), 5000);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={getUserLocation}>Find your location</button>
+      {geolocationError && (
+        <p aria-live="polite">Geolocation is not supported by this browser</p>
+      )}
+    </div>
+  );
+}
