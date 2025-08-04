7d84198505a915df7f35778850c9d8de21286f6f
Alessandro <galloalessandro77@gmail.com>
2025-08-04 17:53:14 +0200

    Fixed route order

--- frontend/src/components/finds-stores-page/store-list/store-routes/StoreRoutes.jsx
+++ frontend/src/components/finds-stores-page/store-list/store-routes/StoreRoutes.jsx
@@ -6,6 +6,7 @@ import noRouteData from "/assets/svg/no-route-data.svg";
 import style from "./StoreRoutes.module.css";
 
 export default function StoreRoutes({ routes }) {
+
   const routeDistanceUnitFormatted = (routeTimeUnit) => {
     let timeName;
 
@@ -55,67 +56,76 @@ export default function StoreRoutes({ routes }) {
       aria-label={`list of routes to get to the store`}
     >
       {routes &&
-        Object.entries(routes).map(([mode, data], index) => (
-          <div
-            key={index}
-            className={style.routeContainer}
-            role="group"
-            aria-label={`type of route ${mode}`}
-          >
-            <div className={style.routeDistanceContainer}>
-              <img src={routeSvg(mode)} className={style.routeTypeSvg} />
-              <div
-                className={style.routeDistance}
-                aria-label={`distance ${data.distanceMeters.distance} ${data.distanceMeters.distanceUnit}`}
-              >
-                {data.distanceMeters.distance !== "no-data" && (
-                  <>
-                    <p aria-hidden="true">{data.distanceMeters.distance}</p>
-                    <p className={style.routeDistanceUnit} aria-hidden="true">
-                      {data.distanceMeters.distanceUnit}
-                    </p>
-                  </>
-                )}
-                {data.distanceMeters.distance === "no-data" && (
-                  <img
-                    src={noRouteData}
-                    className={style.routeTypeSvg}
-                    alt="distance not available"
-                  />
-                )}
-              </div>
-            </div>
+        ["walk", "bicycle", "transit", "drive"]
+          .filter((mode) => routes[mode])
+          .map((mode) => {
+            const data = routes[mode];
 
-            <div
-              className={style.routeDurationContainer}
-              aria-label={`travel time`}
-            >
-              {data.duration &&
-                Object.entries(data.duration).map(([mode, data], index) => (
+            return (
+              <div
+                key={mode}
+                className={style.routeContainer}
+                role="group"
+                aria-label={`type of route ${mode}`}
+              >
+                <div className={style.routeDistanceContainer}>
+                  <img src={routeSvg(mode)} className={style.routeTypeSvg} />
                   <div
-                    key={index}
-                    className={style.routeDuration}
-                    aria-label={`${data} ${mode}`}
+                    className={style.routeDistance}
+                    aria-label={`distance ${data.distanceMeters.distance} ${data.distanceMeters.distanceUnit}`}
                   >
-                    {data !== "no-data" && (
+                    {data.distanceMeters.distance !== "no-data" && (
                       <>
-                        <p aria-hidden="true">{data}</p>
+                        <p aria-hidden="true">{data.distanceMeters.distance}</p>
                         <p
-                          className={style.routeDurationUnit}
+                          className={style.routeDistanceUnit}
                           aria-hidden="true"
                         >
-                          {routeDistanceUnitFormatted(mode)}
+                          {data.distanceMeters.distanceUnit}
                         </p>
                       </>
                     )}
-                    {data === "no-data" && (
-                      <p aria-hidden="true">unknown timing</p>
+                    {data.distanceMeters.distance === "no-data" && (
+                      <img
+                        src={noRouteData}
+                        className={style.routeTypeSvg}
+                        alt="distance not available"
+                      />
                     )}
                   </div>
-                ))}
-            </div>
-          </div>
-        ))}
+                </div>
+
+                <div
+                  className={style.routeDurationContainer}
+                  aria-label={`travel time`}
+                >
+                  {data.duration &&
+                    Object.entries(data.duration).map(([mode, data], index) => (
+                      <div
+                        key={index}
+                        className={style.routeDuration}
+                        aria-label={`${data} ${mode}`}
+                      >
+                        {data !== "no-data" && (
+                          <>
+                            <p aria-hidden="true">{data}</p>
+                            <p
+                              className={style.routeDurationUnit}
+                              aria-hidden="true"
+                            >
+                              {routeDistanceUnitFormatted(mode)}
+                            </p>
+                          </>
+                        )}
+                        {data === "no-data" && (
+                          <p aria-hidden="true">unknown timing</p>
+                        )}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            );
+          })}
     </div>
   );
 }
