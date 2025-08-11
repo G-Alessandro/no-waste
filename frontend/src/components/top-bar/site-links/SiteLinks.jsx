6dbf9b3bab451ccf958b38a27126d67ede7200e9
Alessandro <galloalessandro77@gmail.com>
2025-08-11 17:49:50 +0200

    Added SiteLinks

---
+++ frontend/src/components/top-bar/site-links/SiteLinks.jsx
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import UserDataLogout from "./user-data-logout/UserDataLogout";
+import NoWasteLogo from "/assets/images/png/no-waste-logo.png";
+import DropDownMenu from "/assets/images/svg/dropdown-menu.svg";
+import style from "./SiteLinks.module.css";
+
+export default function SiteLinks({
+  userData,
+  setUserData,
+  setUserId,
+  topBarLocation,
+}) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
+  const [showNav, setShowNav] = useState(isMobile ? false : true);
+  const [rotateDropdownMenu, setRotateDropdownMenu] = useState(false);
+  const [logoutMessage, setLogoutMessage] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 750);
+      window.innerWidth > 750 ? setShowNav(true) : setShowNav(false);
+      if (window.innerWidth <= 750) {
+        setRotateDropdownMenu(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleDropDownMenuClick = () => {
+    setShowNav(!showNav);
+    setRotateDropdownMenu(!rotateDropdownMenu);
+  };
+
+  const handleMobileLinkClick = () => {
+    if (isMobile) {
+      setShowNav(false);
+      setRotateDropdownMenu(false);
+    }
+  };
+  return (
+    <header>
+      <Link
+        to="/"
+        className={style.noWasteLogoLink}
+        aria-disabled={isMobile ? "true" : "false"}
+        aria-label="link to home"
+        onClick={(e) => isMobile && e.preventDefault()}
+      >
+        <img src={NoWasteLogo} />
+        <h1>
+          <span className={style.noWastePart1}>N</span>
+          <span className={style.noWastePart2}>o</span>
+          <span className={style.noWastePart3}>W</span>
+          <span className={style.noWastePart4}>aste</span>
+        </h1>
+      </Link>
+      {isMobile && (
+        <button
+          aria-label="Click to show site navigation links"
+          className={`${style.dropDownMenuBtn} ${
+            rotateDropdownMenu ? style.rotated : ""
+          }`}
+          onClick={() => handleDropDownMenuClick()}
+        >
+          <img src={DropDownMenu} />
+        </button>
+      )}
+      {showNav && (
+        <nav>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={
+                  topBarLocation === "home" ? style.highlightedLink : ""
+                }
+                onClick={() => handleMobileLinkClick()}
+              >
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/finds-stores"
+                className={
+                  topBarLocation === "finds-stores" ? style.highlightedLink : ""
+                }
+                onClick={() => handleMobileLinkClick()}
+              >
+                FINDS STORES
+              </Link>
+            </li>
+            {!userData && (
+              <>
+                <li>
+                  <Link
+                    to="/login"
+                    className={
+                      topBarLocation === "login" ? style.highlightedLink : ""
+                    }
+                    onClick={() => handleMobileLinkClick()}
+                  >
+                    SIGN-IN
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/registration"
+                    className={
+                      topBarLocation === "registration"
+                        ? style.highlightedLink
+                        : ""
+                    }
+                    onClick={() => handleMobileLinkClick()}
+                  >
+                    SIGN-UP
+                  </Link>
+                </li>
+              </>
+            )}
+            <UserDataLogout
+              userData={userData}
+              setUserData={setUserData}
+              setUserId={setUserId}
+              setLogoutMessage={setLogoutMessage}
+              handleMobileLinkClick={handleMobileLinkClick}
+              topBarLocation={topBarLocation}
+            />
+          </ul>
+        </nav>
+      )}
+      {logoutMessage && (
+        <p aria-live="polite" className={style.logoutMessage}>
+          {logoutMessage}
+        </p>
+      )}
+    </header>
+  );
+}
