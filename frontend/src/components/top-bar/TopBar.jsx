d861f7ca4bfae0668026f02cf3af9cbde957a3c0
Alessandro <galloalessandro77@gmail.com>
2025-08-11 16:38:53 +0200

    Removed links logic Added SiteLinks component

--- frontend/src/components/top-bar/TopBar.jsx
+++ frontend/src/components/top-bar/TopBar.jsx
@@ -1,12 +1,8 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import NoWasteLogo from "/assets/images/png/no-waste-logo.png";
-import style from "./TopBar.module.css";
+import SiteLinks from "./site-links/SiteLinks";
 
-export default function TopBar({ setUserId }) {
+export default function TopBar({ setUserId, topBarLocation }) {
   const [userData, setUserData] = useState(null);
-  const [showLoader, setShowLoader] = useState(false);
-  const [logoutMessage, setLogoutMessage] = useState(null);
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -44,104 +40,14 @@ export default function TopBar({ setUserId }) {
     fetchUserData();
   }, []);
 
-  const handleLogout = async (event) => {
-    event.preventDefault();
-    setShowLoader(true);
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/authentication/logout`,
-        {
-          method: "POST",
-          credentials: "include",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            accessToken: localStorage.getItem("accessToken"),
-          }),
-        }
-      );
-      const data = await response.json();
-      if (!response.ok) {
-        setLogoutMessage(data.error);
-      } else {
-        localStorage.removeItem("accessToken");
-        setUserData(null);
-        setUserId(null);
-        setLogoutMessage(data.message);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setShowLoader(false);
-      setTimeout(() => setLogoutMessage(null), 5000);
-    }
-  };
-
   return (
-    <header>
-      <Link to="/" className={style.noWasteLogoLink}>
-        <img src={NoWasteLogo} alt="link to home" />
-        <h1>
-          <span className={style.noWastePart1}>N</span>
-          <span className={style.noWastePart2}>o</span>
-          <span className={style.noWastePart3}>W</span>
-          <span className={style.noWastePart4}>aste</span>
-        </h1>
-      </Link>
-      <nav>
-        <ul>
-          <>
-            <li>
-              <Link to="/">HOME</Link>
-            </li>
-            <li>
-              <Link to="/finds-stores">FINDS STORES</Link>
-            </li>
-            {!userData && (
-              <>
-                <li>
-                  <Link to="/login">SIGN-IN</Link>
-                </li>
-                <li>
-                  <Link to="/registration">SIGN-UP</Link>
-                </li>
-              </>
-            )}
-          </>
-          {userData && (
-            <div className={style.userDataLogoutContainer}>
-              <li className={style.userData}>
-                <p>{`${userData.firstName} ${userData.lastName}`}</p>
-              </li>
-              <li>
-                {!showLoader && (
-                  <button
-                    onClick={handleLogout}
-                    type="button"
-                    className={style.logoutBtn}
-                  >
-                    logout
-                  </button>
-                )}
-                {showLoader && (
-                  <div
-                    aria-live="polite"
-                    aria-label="Logging out, please wait..."
-                    className={style.loader}
-                  ></div>
-                )}
-              </li>
-            </div>
-          )}
-        </ul>
-        {logoutMessage && (
-          <p aria-live="polite" className={style.logoutMessage}>
-            {logoutMessage}
-          </p>
-        )}
-      </nav>
-    </header>
+    <>
+      <SiteLinks
+        userData={userData}
+        setUserData={setUserData}
+        setUserId={setUserId}
+        topBarLocation={topBarLocation}
+      />
+    </>
   );
 }
