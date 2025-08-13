096378575e04062a8c609f24e84e3f2e7767af96
Alessandro <galloalessandro77@gmail.com>
2025-08-13 17:55:28 +0200

    Removed demo account logic and button

--- frontend/src/components/login-page/Login.jsx
+++ frontend/src/components/login-page/Login.jsx
@@ -11,25 +11,19 @@ export default function Login() {
   const [showLoader, setShowLoader] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = async (event, type) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     setShowLoader(true);
 
-    const isLogin = type === "login";
-    const route = isLogin ? "login" : "demo-account";
-    const method = isLogin ? "POST" : "GET";
-
-    const formData = isLogin
-      ? JSON.stringify({
-          email: event.target.email.value,
-          password: event.target.password.value,
-        })
-      : undefined;
+    const formData = JSON.stringify({
+      email: event.target.email.value,
+      password: event.target.password.value,
+    });
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/authentication/${route}`,
+        `${import.meta.env.VITE_BACKEND_URL}/authentication/login`,
         {
-          method: method,
+          method: "POST",
           credentials: "include",
           headers: { "Content-Type": "application/json" },
           mode: "cors",
@@ -70,65 +64,66 @@ export default function Login() {
   }, [loginSuccessful]);
 
   return (
-    <main>
-      <TopBar />
-      {loginSuccessful && (
-        <div aria-live="polite" className={style.loginSuccessful}>
-          <p>
+    <>
+      <main className={style.loginMain}>
+        <TopBar topBarLocation={"login"} />
+        {loginSuccessful && (
+          <p aria-live="polite" className={style.loginSuccessful}>
             Login successful, you will be redirected to the page to find stores
             near you
           </p>
-        </div>
-      )}
-      {!loginSuccessful && (
-        <form
-          onSubmit={(event) => handleSubmit(event, "login")}
-          className={style.loginForm}
-        >
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" minLength={1} required />
+        )}
+        {loginError && (
+          <p aria-live="polite" className={style.loginError}>
+            Incorrect email or password
+          </p>
+        )}
+        {fetchError && (
+          <p aria-live="polite" className={style.fetchError}>
+            An error occurred while logging in
+          </p>
+        )}
 
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            minLength={8}
-            required
-          />
-          {showLoader && (
-            <div
-              aria-live="assertive"
-              aria-label="Logging in, please wait..."
-              className={style.loader}
-            ></div>
-          )}
-          {!showLoader && (
-            <button type="submit" className={style.loginBtn}>
-              Login
-            </button>
-          )}
-          {!showLoader && (
-            <button
-              onClick={(event) => handleSubmit(event, "demo-account")}
-              className={style.tryDemoAccountBtn}
-            >
-              Try a demo account
-            </button>
-          )}
-        </form>
-      )}
-      {loginError && (
-        <div aria-live="polite" className={style.loginError}>
-          <p>Incorrect email or password</p>
-        </div>
-      )}
-      {fetchError && (
-        <div className={style.fetchError}>
-          <p>An error occurred while logging in.</p>
-        </div>
-      )}
+        {!loginSuccessful && (
+          <form
+            onSubmit={(event) => handleSubmit(event)}
+            className={style.loginForm}
+          >
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              minLength={1}
+              placeholder="Enter your email"
+              required
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              minLength={8}
+              placeholder="Enter your password"
+              required
+            />
+            {showLoader && (
+              <div
+                aria-live="assertive"
+                aria-label="Logging in, please wait"
+                className={style.loader}
+              ></div>
+            )}
+            {!showLoader && (
+              <button type="submit" className={style.loginBtn}>
+                Login
+              </button>
+            )}
+          </form>
+        )}
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
