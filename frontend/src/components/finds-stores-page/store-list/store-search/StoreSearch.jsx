ae54774209978dbb3225472ca345838dd59800d3
Alessandro <galloalessandro77@gmail.com>
2025-08-17 20:14:08 +0200

    Added style logic

--- frontend/src/components/finds-stores-page/store-list/store-search/StoreSearch.jsx
+++ frontend/src/components/finds-stores-page/store-list/store-search/StoreSearch.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
+import SearchSvg from "/assets/images/svg/store-search/search.svg";
+import style from "./StoreSearch.module.css";
+
 export default function StoreSearch({ setSearchText }) {
-    
+  const [focused, setFocused] = useState(false);
+
   let searchHandler = (e) => {
     let lowerCase = e.target.value.toLowerCase();
     setSearchText(lowerCase);
   };
 
   return (
-    <>
-      <label htmlFor="store-search">Search</label>
+    <div className={style.storeSearch}>
       <input
         type="text"
         onChange={searchHandler}
         name="store-search"
         id="store-search"
+        className={style.searchInput}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
       />
-    </>
+      <label
+        htmlFor="store-search"
+        aria-label="search store"
+        className={`${style.searchLabel} ${
+          focused ? style.searchLabelFocus : ""
+        }`}
+      >
+        <img src={SearchSvg} />
+      </label>
+    </div>
   );
 }
