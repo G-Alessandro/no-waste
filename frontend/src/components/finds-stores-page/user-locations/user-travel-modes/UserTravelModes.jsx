b0ee2e423d4e2505b9d06a72a5f1ee0889e8c421
Alessandro <galloalessandro77@gmail.com>
2025-08-19 18:42:33 +0200

    Improved logic Added className

--- frontend/src/components/finds-stores-page/user-locations/user-travel-modes/UserTravelModes.jsx
+++ frontend/src/components/finds-stores-page/user-locations/user-travel-modes/UserTravelModes.jsx
@@ -1,3 +1,24 @@
+import style from "./UserTravelModes.module.css";
+
+const modes = [
+  {
+    name: "Walking",
+    value: "walk",
+  },
+  {
+    name: "Bicycle",
+    value: "bicycle",
+  },
+  {
+    name: "Car",
+    value: "drive",
+  },
+  {
+    name: "Public Transport",
+    value: "transit",
+  },
+];
+
 export default function UserTravelModes({ travelMode, setTravelMode }) {
   const handleLocationChange = (event) => {
     const value = event.target.value;
@@ -5,7 +26,7 @@ export default function UserTravelModes({ travelMode, setTravelMode }) {
   };
 
   return (
-    <>
+    <div className={style.userLocationsSelect}>
       <label htmlFor="user-travel-modes">Travel Mode</label>
       <select
         id="user-travel-modes"
@@ -13,11 +34,10 @@ export default function UserTravelModes({ travelMode, setTravelMode }) {
         defaultValue={travelMode}
         onChange={handleLocationChange}
       >
-        <option value="walk">Walking</option>
-        <option value="bicycle">Bicycle</option>
-        <option value="drive">Car</option>
-        <option value="transit">Public Transport</option>
+        {modes.map((mode) => {
+          return <option value={mode.value}>{mode.name}</option>;
+        })}
       </select>
-    </>
+    </div>
   );
 }
