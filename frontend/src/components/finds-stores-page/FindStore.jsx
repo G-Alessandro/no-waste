f9039a813a0c4af14465ac047e2e884bc42a9add
Alessandro <galloalessandro77@gmail.com>
2025-08-25 12:39:43 +0200

    Removed AddStore and AddStoreItem components

--- frontend/src/components/finds-stores-page/FindStore.jsx
+++ frontend/src/components/finds-stores-page/FindStore.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import TopBar from "../top-bar/TopBar";
-import AddStore from "./add-store/AddStore.jsx";
-import AddStoreItem from "./add-store-item/AddStoreItem.jsx";
 import StoreList from "./store-list/StoreList.jsx";
 import UserLocations from "./user-locations/UserLocations.jsx";
 import CustomMap from "./google-maps/CustomMap.jsx";
@@ -36,72 +34,65 @@ export default function FindStore() {
   }, [message, error]);
 
   return (
-    <main className={style.FindStoreContainer}>
-      <TopBar setUserId={setUserId} />
-      <section>
-        {message && <p>{message}</p>}
-        {error && <p>{error}</p>}
-        {userId && (
-          <>
-            <AddStore
-              newStoreLocation={newStoreLocation}
-              setNewStoreLocation={setNewStoreLocation}
-              statusChanged={statusChanged}
-              setStatusChanged={setStatusChanged}
-              setMessage={setMessage}
-              setError={setError}
-              addingLocationFromMap={addingLocationFromMap}
-              setAddingLocationFromMap={setAddingLocationFromMap}
-            />
-
-            <AddStoreItem
-              statusChanged={statusChanged}
-              setStatusChanged={setStatusChanged}
+    <>
+      <main className={style.findStoreMain}>
+        <TopBar setUserId={setUserId} topBarLocation={"finds-stores"} />
+        <div className={style.sectionContainer}>
+          {message && <p aria-live="polite">{message}</p>}
+          {error && <p aria-live="polite">{error}</p>}
+          <section aria-labelledby="store-management-heading">
+            <h2 id="store-management-heading" className={style.srOnly}>
+              Manage the addition of new stores, items and view current stores
+            </h2>
+            <StoreList
               userId={userId}
               storesList={storesList}
+              setStoresList={setStoresList}
+              sortedStoresList={sortedStoresList}
+              setSortedStoresList={setSortedStoresList}
+              userLocation={userLocation}
+              newStoreLocation={newStoreLocation}
+              setNewStoreLocation={setNewStoreLocation}
+              addingLocationFromMap={addingLocationFromMap}
+              setAddingLocationFromMap={setAddingLocationFromMap}
               setMessage={setMessage}
               setError={setError}
+              selectedStore={selectedStore}
+              setSelectedStore={setSelectedStore}
+              statusChanged={statusChanged}
+              setStatusChanged={setStatusChanged}
             />
-          </>
-        )}
-        <StoreList
-          userId={userId}
-          storesList={storesList}
-          setStoresList={setStoresList}
-          sortedStoresList={sortedStoresList}
-          setSortedStoresList={setSortedStoresList}
-          userLocation={userLocation}
-          selectedStore={selectedStore}
-          setSelectedStore={setSelectedStore}
-          statusChanged={statusChanged}
-          setStatusChanged={setStatusChanged}
-        />
-      </section>
-      <section>
-        <UserLocations
-          userId={userId}
-          setMessage={setMessage}
-          setError={setError}
-          userLocation={userLocation}
-          setUserLocation={setUserLocation}
-          setShowUserMarker={setShowUserMarker}
-          travelMode={travelMode}
-          setTravelMode={setTravelMode}
-        />
-        <CustomMap
-          selectedStore={selectedStore}
-          setSelectedStore={setSelectedStore}
-          userLocation={userLocation}
-          storesList={storesList}
-          newStoreLocation={newStoreLocation}
-          setNewStoreLocation={setNewStoreLocation}
-          addingLocationFromMap={addingLocationFromMap}
-          showUserMarker={showUserMarker}
-          setShowUserMarker={setShowUserMarker}
-          travelMode={travelMode}
-        />
-      </section>
+          </section>
+          <section aria-labelledby="map-section-heading">
+            <h2 id="map-section-heading" className={style.srOnly}>
+              Map and user location
+            </h2>
+            <UserLocations
+              userId={userId}
+              setMessage={setMessage}
+              setError={setError}
+              userLocation={userLocation}
+              setUserLocation={setUserLocation}
+              setShowUserMarker={setShowUserMarker}
+              travelMode={travelMode}
+              setTravelMode={setTravelMode}
+            />
+            <CustomMap
+              selectedStore={selectedStore}
+              setSelectedStore={setSelectedStore}
+              userLocation={userLocation}
+              storesList={storesList}
+              newStoreLocation={newStoreLocation}
+              setNewStoreLocation={setNewStoreLocation}
+              addingLocationFromMap={addingLocationFromMap}
+              showUserMarker={showUserMarker}
+              setShowUserMarker={setShowUserMarker}
+              travelMode={travelMode}
+            />
+          </section>
+        </div>
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
