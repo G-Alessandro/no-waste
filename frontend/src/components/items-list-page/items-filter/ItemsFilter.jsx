8c3e17d3814491a5731ac0e77c5e1afa75866185
Alessandro <galloalessandro77@gmail.com>
2025-08-27 15:28:51 +0200

    Added className

--- frontend/src/components/items-list-page/items-filter/ItemsFilter.jsx
+++ frontend/src/components/items-list-page/items-filter/ItemsFilter.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import style from "./ItemsFilter.module.css";
 
 const numericFilters = [
   { label: "Price", attribute: "price", parameter: "price" },
@@ -68,48 +69,56 @@ export default function ItemsFilter({
   };
 
   return (
-    <div>
-      <label htmlFor="items-search">Search</label>
-      <input
-        type="text"
-        onChange={searchHandler}
-        name="items-search"
-        id="items-search"
-      />
-
-      <label htmlFor="select-type">Type</label>
-      <select
-        name="select-type"
-        id="select-type"
-        defaultValue="none"
-        onChange={(e) => setSelectedType(e.target.value)}
-      >
-        {typesFilter.map((type) => {
-          return (
-            <option value={type} key={type}>
-              {type}
-            </option>
-          );
-        })}
-      </select>
+    <div className={style.itemsFilterContainer}>
+      <div className={style.labelInputContainer}>
+        <label htmlFor="items-search">Search</label>
+        <input
+          type="text"
+          onChange={searchHandler}
+          placeholder="Enter item name"
+          name="items-search"
+          id="items-search"
+        />
+      </div>
+      <div className={style.labelInputContainer}>
+        <label htmlFor="select-type">Type</label>
+        <select
+          name="select-type"
+          id="select-type"
+          defaultValue="none"
+          onChange={(e) => setSelectedType(e.target.value)}
+        >
+          {typesFilter.map((type) => {
+            return (
+              <option value={type} key={type}>
+                {type}
+              </option>
+            );
+          })}
+        </select>
+      </div>
 
       {numericFilters.map((filter) => {
         return (
           <React.Fragment key={filter.parameter}>
-            <label htmlFor={`items-${filter.attribute}`}>{filter.label}</label>
-            <select
-              name={`items-${filter.attribute}`}
-              id={`items-${filter.attribute}`}
-              onChange={(e) =>
-                handleNumericFilters(filter.parameter, e.target.value)
-              }
-              ref={(el) => (selectRefs.current[filter.parameter] = el)}
-              defaultValue="none"
-            >
-              <option value="none">none</option>
-              <option value="ascending">ascending</option>
-              <option value="descending">descending</option>
-            </select>
+            <div className={style.labelInputContainer}>
+              <label htmlFor={`items-${filter.attribute}`}>
+                {filter.label}
+              </label>
+              <select
+                name={`items-${filter.attribute}`}
+                id={`items-${filter.attribute}`}
+                onChange={(e) =>
+                  handleNumericFilters(filter.parameter, e.target.value)
+                }
+                ref={(el) => (selectRefs.current[filter.parameter] = el)}
+                defaultValue="none"
+              >
+                <option value="none">None</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </div>
           </React.Fragment>
         );
       })}
