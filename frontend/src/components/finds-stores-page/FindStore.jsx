e59aa7b52e42ccd397b781c44eceeb95cf4b5c8c
Alessandro <galloalessandro77@gmail.com>
2025-09-01 16:31:25 +0200

    Added logic for mobile responsiveness

--- frontend/src/components/finds-stores-page/FindStore.jsx
+++ frontend/src/components/finds-stores-page/FindStore.jsx
@@ -22,6 +22,24 @@ export default function FindStore() {
   const [showUserMarker, setShowUserMarker] = useState(true);
   const [message, setMessage] = useState(null);
   const [error, setError] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
+  const [showStoreList, setShowStoreList] = useState(true);
+  const [showUserLocations, setShowUserLocations] = useState(true);
+  const [showCustomMap, setShowCustomMap] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 750);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (isMobile) {
+      setShowUserLocations(false);
+    }
+  }, [isMobile]);
 
   useEffect(() => {
     let timerId;
@@ -33,14 +51,32 @@ export default function FindStore() {
     return () => clearTimeout(timerId);
   }, [message, error]);
 
+  const handleShowUserLocationsOptionsClick = () => {
+    setShowUserLocations(!showUserLocations);
+    setShowCustomMap(!showCustomMap);
+  };
+
   return (
     <>
       <main className={style.findStoreMain}>
         <TopBar setUserId={setUserId} topBarLocation={"finds-stores"} />
         <div className={style.sectionContainer}>
-          {message && <p aria-live="polite" className={style.message}>{message}</p>}
-          {error && <p aria-live="polite" className={style.error}>{error}</p>}
-          <section aria-labelledby="store-management-heading">
+          {message && (
+            <p aria-live="polite" className={style.message}>
+              {message}
+            </p>
+          )}
+          {error && (
+            <p aria-live="polite" className={style.error}>
+              {error}
+            </p>
+          )}
+
+          <section
+            aria-labelledby="store-management-heading"
+            className={style.storeSection}
+            style={isMobile ? { display: showStoreList ? "flex" : "none" } : {}}
+          >
             <h2 id="store-management-heading" className={style.srOnly}>
               Manage the addition of new stores, items and view current stores
             </h2>
@@ -63,32 +99,79 @@ export default function FindStore() {
               setStatusChanged={setStatusChanged}
             />
           </section>
-          <section aria-labelledby="map-section-heading">
+
+          <div
+            className={`${style.mobileDropdown}  ${
+              showStoreList ? style.mobileDropdown : style.mobileDropdownLeft
+            }`}
+          >
+            <button
+              onClick={() => setShowStoreList(!showStoreList)}
+              className={`${style.mobileDropdownBtn}  ${
+                showStoreList ? "" : style.mobileDropdownLeftBtn
+              } 
+               ${
+                 addingLocationFromMap ? (showStoreList ? style.blink : "") : ""
+               }
+              ${
+                newStoreLocation &&
+                showStoreList === false &&
+                addingLocationFromMap === false
+                  ? style.blink
+                  : ""
+              }
+              `}
+            >
+              {showStoreList ? "SEE THE MAP" : "SEE THE STORE LIST"}
+            </button>
+          </div>
+
+          <section
+            aria-labelledby="map-section-heading"
+            className={style.mapSection}
+            style={
+              isMobile ? { display: !showStoreList ? "flex" : "none" } : {}
+            }
+          >
             <h2 id="map-section-heading" className={style.srOnly}>
               Map and user location
             </h2>
-            <UserLocations
-              userId={userId}
-              setMessage={setMessage}
-              setError={setError}
-              userLocation={userLocation}
-              setUserLocation={setUserLocation}
-              setShowUserMarker={setShowUserMarker}
-              travelMode={travelMode}
-              setTravelMode={setTravelMode}
-            />
-            <CustomMap
-              selectedStore={selectedStore}
-              setSelectedStore={setSelectedStore}
-              userLocation={userLocation}
-              storesList={storesList}
-              newStoreLocation={newStoreLocation}
-              setNewStoreLocation={setNewStoreLocation}
-              addingLocationFromMap={addingLocationFromMap}
-              showUserMarker={showUserMarker}
-              setShowUserMarker={setShowUserMarker}
-              travelMode={travelMode}
-            />
+
+            {isMobile && (
+              <button
+                className={style.showUserLocationsOptionBtn}
+                onClick={() => handleShowUserLocationsOptionsClick()}
+              >
+                {showUserLocations ? "CLOSE OPTIONS" : " SHOW LOCATIONS OPTION"}
+              </button>
+            )}
+
+            {showUserLocations && (
+              <UserLocations
+                userId={userId}
+                setMessage={setMessage}
+                setError={setError}
+                userLocation={userLocation}
+                setUserLocation={setUserLocation}
+                setShowUserMarker={setShowUserMarker}
+                travelMode={travelMode}
+                setTravelMode={setTravelMode}
+              />
+            )}
+            {showCustomMap && (
+              <CustomMap
+                selectedStore={selectedStore}
+                setSelectedStore={setSelectedStore}
+                userLocation={userLocation}
+                storesList={storesList}
+                newStoreLocation={newStoreLocation}
+                setNewStoreLocation={setNewStoreLocation}
+                addingLocationFromMap={addingLocationFromMap}
+                showUserMarker={showUserMarker}
+                setShowUserMarker={setShowUserMarker}
+                travelMode={travelMode}
+              />
+            )}
           </section>
         </div>
       </main>
